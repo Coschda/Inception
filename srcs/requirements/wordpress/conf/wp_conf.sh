c1aa0471f9f5c8808ed5961fe12f3d3fbca33483
Mikail Doumi <mdoumi@k2r1p6.42mulhouse.fr>
2024-06-29 14:28:25 +0200

    modif

--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -2,37 +2,25 @@
 
 sleep 15
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    cd "/var/www/wordpress"
     wp config create    --allow-root \
                         --dbname="$MYSQL_DATABASE" \
                         --dbuser="$MYSQL_USER" \
                         --dbpass="$MYSQL_PASSWORD" \
                         --dbhost=mariadb:3306 \
                         --path='/var/www/wordpress'
+    wp core install     --allow-root\
+                        --path="/var/www/wordpress"\
+                        --url="$LOGIN.42.fr"\
+                        --title="test1"\
+                        --admin_user="$WP_ADMIN"\
+                        --admin_password="$WP_ADMIN_PASSWORD"\
+                        --admin_email="$WP_ADMIN_EMAIL"
+    wp user create "$WP_USER" "$WP_USER_MAIL"\
+                        --allow-root\
+                        --path="/var/www/wordpress"\
+                        --user_pass="$WP_USER_PASSWORD"\
+                        --display_name="$WP_DISPLAY_NAME"\
+                        --role="author"
 fi
 
 /usr/sbin/php-fpm7.3 -F
-
-#if [ -f "$WP_PATH"/wp-config.php ];
-  #then
-    #echo "wordpress is already installed"
-  #else
-    #cd "$WP_PATH"
-    #wp core download --allow-root --locale=fr_FR
-    #wp core install --allow-root\
-                    #--path="$WP_PATH"\
-                    #--url="$WP_URL"\
-                    #--title="$WP_TITLE"\
-                    #--admin_user="$WP_ADMIN"\
-                    #--admin_password="$WP_ADMIN_PASSWORD"\
-                    #--admin_email="$WP_ADMIN_EMAIL"
-    #wp user create "$WP_USER" "$WP_USER_MAIL"\
-                    #--allow-root\
-                    #--path="$WP_PATH"\
-                    #--user_pass="$WP_USER_PASSWORD"\
-                    #--display_name="$WP_DISPLAY_NAME"\
-                    #--role="author"
-#fi
-
-#echo "running php-fpm"
-#/usr/sbin/php-fpm7.3 -F
