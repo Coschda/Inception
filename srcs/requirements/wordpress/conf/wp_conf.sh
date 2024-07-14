cdfc93d1cd5778e508b1429b19372c93f543415f
Ubuntu Maison <mdoumi@DESKTOP-FRSILNV>
2024-07-14 09:58:41 +0200

    wordpress

--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -1,26 +1,32 @@
-#!/bin/bash
-
+echo "waiting for mariadb"
 sleep 15
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    wp config create    --allow-root \
-                        --dbname="$MYSQL_DATABASE" \
-                        --dbuser="$MYSQL_USER" \
-                        --dbpass="$MYSQL_PASSWORD" \
-                        --dbhost=mariadb:3306 \
-                        --path='/var/www/wordpress'
-    wp core install     --allow-root\
-                        --path="/var/www/wordpress"\
-                        --url="$LOGIN.42.fr"\
-                        --title="test1"\
-                        --admin_user="$WP_ADMIN"\
-                        --admin_password="$WP_ADMIN_PASSWORD"\
-                        --admin_email="$WP_ADMIN_EMAIL"
+
+if [ -f "$WP_PATH"/wp-config.php ];
+  then
+    echo "wordpress is already installed"
+  else
+    cd "$WP_PATH"
+    wp core download --allow-root --locale=fr_FR
+    wp config create --allow-root\
+                    --dbname="$MYSQL_DATABASE"\
+                    --dbuser="$MYSQL_USER"\
+                    --dbpass="$MYSQL_PASSWORD"\
+                    --dbhost="$MYSQL_DB_NAME"\
+                    --dbcharset="utf8"
+    wp core install --allow-root\
+                    --path="$WP_PATH"\
+                    --url="$WP_URL"\
+                    --title="$WP_TITLE"\
+                    --admin_user="$WP_ADMIN"\
+                    --admin_password="$WP_ADMIN_PASSWORD"\
+                    --admin_email="$WP_ADMIN_EMAIL"
     wp user create "$WP_USER" "$WP_USER_MAIL"\
-                        --allow-root\
-                        --path="/var/www/wordpress"\
-                        --user_pass="$WP_USER_PASSWORD"\
-                        --display_name="$WP_DISPLAY_NAME"\
-                        --role="author"
+                    --allow-root\
+                    --path="$WP_PATH"\
+                    --user_pass="$WP_USER_PASSWORD"\
+                    --display_name="$WP_DISPLAY_NAME"\
+                    --role="author"
 fi
 
+echo "running php-fpm"
 /usr/sbin/php-fpm7.3 -F
